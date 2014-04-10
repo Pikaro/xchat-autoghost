c68463a82a16c4237f1fbfd58b9256480bdea125
Pikaro <git@d-reis.com>
2014-04-10 16:55:48 +0200

    fixed bug with rejoining voice-only channels

--- autoghost.py
+++ autoghost.py
@@ -6,8 +6,8 @@ __module_description__ = "Ghost old session automatically."
 
 display = (__module_name__ + " " + __module_version__ + " has been loaded.",)
 
-SUCCESS_RETURN = xchat.EAT_NONE
-FAIL_RETURN = xchat.EAT_NONE
+SUCCESS = xchat.EAT_ALL
+FAIL = xchat.EAT_NONE
 
 def lineprint(line):
     print("\0034autoghost: " + line + "\003")
@@ -17,8 +17,8 @@ def nickchange(word, word_eol, userdata):
 	password = xchat.get_info("nickserv")
 	if xchat.get_info("nick") is desired_nick:
 		lineprint("Got desired nick now: "+desired_nick)
-		return SUCCESS_RETURN
-	return FAIL_RETURN
+		return SUCCESS
+	return FAIL
 
 def ghost(word, word_eol, userdata):
 	desired_nick = xchat.get_prefs("irc_nick1")
@@ -27,9 +27,9 @@ def ghost(word, word_eol, userdata):
 	if password:
 		lineprint("Attempting to ghost old session.")
 		xchat.command("msg nickserv ghost "+desired_nick+" "+password)
-		return SUCCESS_RETURN
+		return SUCCESS
 	lineprint("But we have no password!")
-	return FAIL_RETURN
+	return FAIL
 
 def notice(word, word_eol, userdata):
 	password = xchat.get_info("nickserv")
@@ -39,24 +39,24 @@ def notice(word, word_eol, userdata):
 		if password:
 			lineprint("Registered nickname. Attempting to auto-identify.")
 			xchat.command("msg nickserv identify "+password)
-			return SUCCESS_RETURN
+			return SUCCESS
 		lineprint("Registered nickname, but we have no password!")
-		return FAIL_RETURN
+		return FAIL
 
 	elif "has been ghosted" in word_eol[0] or "Ghost with your nick" in word_eol[0]:
 		lineprint("Ghosting successful. Setting nick to "+desired_nick)
 		xchat.command("nick "+desired_nick)
-		return SUCCESS_RETURN
+		return SUCCESS
 	
 	elif "is not a registered nickname." in word_eol[0] or "Your nick isn't registered." in word_eol[0]:
 		lineprint("Not using desired nick. Setting nick to "+desired_nick)
 		xchat.command("nick "+desired_nick)
-		return SUCCESS_RETURN
+		return SUCCESS
 
 	elif "You are already identified." in word_eol[0] or "You are already logged in" in word_eol[0]:
-		return SUCCESS_RETURN
+		return SUCCESS
 
-	return FAIL_RETURN
+	return FAIL
 
 def servertext(word, word_eol, userdata):
 	if "ickname" in word_eol[0] and "in use" in word_eol[0]:
@@ -64,14 +64,14 @@ def servertext(word, word_eol, userdata):
 
 	elif "while banned on channel" in word_eol[0]:
 		for w in word:
-			if '\#' in w:
-				lineprint("Channel "+word+" prevents unregistered users from nick changes. Leaving, changing nick, rejoining.")
-				xchat.command("leave "+word)
+			if '#' in w:
+				lineprint("Channel "+w+" prevents unregistered users from nick changes. Leaving, changing nick, rejoining.")
+				xchat.command("leave "+w)
 				xchat.command("nick "+xchat.get_prefs("irc_nick1"))
-				xchat.command("join "+word)
-				return SUCCESS_RETURN
+				xchat.command("join "+w)
+				return SUCCESS
 	
-	return FAIL_RETURN
+	return FAIL
 
 for line in display:
 	lineprint(line)
